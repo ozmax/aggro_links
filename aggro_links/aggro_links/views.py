31787f5ae55bdbbd212c3c02cd4949c704703a32
Georgios Tsakalos <ozmax.planet@gmail.com>
2015-07-21 15:59:05 +0300

    add view for GET user activation

---
+++ aggro_links/aggro_links/views.py
@@ -0,0 +1,21 @@
+import requests
+import urllib
+from django.views.decorators.http import require_http_methods
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+
+@require_http_methods(["GET", ])
+def activation_frontend(request, uid=None, token=None):
+    if uid and token:
+        payload = {
+            'uid': uid,
+            'token': token
+            }
+        enc = urllib.urlencode(payload)
+        url = 'http://localhost:8000/auth/activate/'
+        headers =  {'content-type': 'application/x-www-form-urlencoded'}
+        response = requests.post(url, data=enc, headers=headers)
+        if response.status_code == 200:
+            return HttpResponse("ACTIVATED")
+        else:
+            return HttpResponse(response.text)
