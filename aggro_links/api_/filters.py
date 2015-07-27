e4947767a2c6e84e84e33a7758fcf6c4f3a1ccdc
Georgios Tsakalos <tsakalos@grnet.gr>
2015-07-27 18:58:55 +0300

    fix bug in global object filtering
    handle unauthenticated users - raise error

--- aggro_links/api_/filters.py
+++ aggro_links/api_/filters.py
@@ -1,5 +1,9 @@
 from rest_framework.filters import BaseFilterBackend
+from rest_framework.serializers import ValidationError
 
 class IsOwnerFilter(BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
-        return queryset.filter(owner=request.user)
+        if request.user.is_authenticated():
+            return queryset.filter(owner=request.user)
+        else:
+            raise ValidationError({"detail": "No credentials provided."})
